f43bc78d986b12c57de600ca40ead1d08b78bb60
Sahal Amoud <sahalamoud@Sahals-iMac.local>
2024-11-17 15:43:12 -0600

    ticket number

--- version.tf
+++ version.tf
@@ -1,15 +1,12 @@
 terraform {
-required_providers {
-  aws = {
-    source = "harshicorp/aws"
-    version = "4.67.0"
+  required_providers {
+    aws = {
+        source = "harshicorp/aws"
+        version = "4.67.0"
+    }
   }
 }
+
+provider "aws" {
+  region = "us-east-1"
 }
-    
- provider "aws" {
-   region = "us-east-1"
- } 
-  
-
-
