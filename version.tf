d5321117ac1604732c10f60bf9c0eac8ab5308dd
Sahal Amoud <sahalamoud@Sahals-iMac.local>
2025-12-10 12:46:45 -0600

    ticket2

--- version.tf
+++ version.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
-    aws = {
-        source = "harshicorp/aws"
-        version = "4.67.0"
+    aws ={
+      source = "harshisorp/aws"
+      version = "4.67.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region ="us-east-1"
 }
